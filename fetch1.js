3ede0988c628a10492dd42ce7c0aecbe02a0f68a
James Hansen <jameszenartist@gmail.com>
2022-03-20 15:47:28 -0700

    updated fetch functions

--- fetch1.js
+++
@@ -1,8 +0,0 @@
-fetch("https://unsplash.it/600/400")
-  .then((res) => res.blob())
-  .then((blob) => {
-    console.log(blob);
-    let img = document.createElement("img");
-    img.src = URL.createObjectURL(blob);
-    document.querySelector("body").appendChild(img);
-  });
